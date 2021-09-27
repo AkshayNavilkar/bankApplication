1017d852723f217ae6860ff8d4b18e81d20f722b
Jayshree Chaure <89380314+jayshree-chaure@users.noreply.github.com>
2021-09-27 12:45:50 +0530

    Added Account Controller

---
+++ src/main/java/com/bank/app/controller/AccountController.java
@@ -0,0 +1,56 @@
+package com.bank.app.controller;
+
+import com.bank.app.impl.AccountServiceImpl;
+import com.bank.app.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class AccountController {
+
+    @Autowired
+    private AccountServiceImpl accountService;
+
+    @PostMapping("account")
+    public ResponseEntity<Account> createAccount(@Valid @RequestBody Account newAccount) {
+        Account account = accountService.createAccount(newAccount);
+        return new ResponseEntity<>(account, HttpStatus.CREATED);
+    }
+
+    @GetMapping("account/accountNumber/{number}")
+    public ResponseEntity<Account> getAccountByAccountNumber(@PathVariable Integer number) {
+        Account account = accountService.getByAccountNumber(number);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    @PatchMapping("account/deposit/{accountNo}/{balance}")
+    public ResponseEntity<Account> deposit(@Valid @PathVariable Integer accountNo,@PathVariable Float balance) {
+        Account account = accountService.depositBalance(accountNo,balance);
+        return new ResponseEntity<>(account,HttpStatus.OK);
+    }
+
+    @GetMapping("account/userId/{id}")
+    public ResponseEntity<Account> getAccountByUserId(@PathVariable Integer id) {
+        Account accounts = accountService.getByUserId(id);
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @GetMapping("accounts")
+    public ResponseEntity<List<Account>> getAllAccount() {
+        return new ResponseEntity<>(accountService.getAllAccount(), HttpStatus.OK);
+    }
+
+    @GetMapping("account/balance/{userId}/{accountNo}")
+    public float getBalanceOfUser(@PathVariable Integer userId, @PathVariable Integer accountNo) {
+        float balanceOfUser = accountService.getBalanceOfUser(userId, accountNo);
+        return balanceOfUser;
+    }
+
+}
